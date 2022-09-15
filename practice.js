abb2a609e0442a10ff31955cfef8496213384379
Dethorasis <113557686+Dethorasis@users.noreply.github.com>
2022-09-15 12:10:23 +1200

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,1 +1,10 @@
+function classRoomPopulation (students) {
+    return students / 30; //Very basic function to return a number of students divided by 30
+}
+
+console.log(`If you have 12 students out of a classroom size of
+30 students, the total percentage of students attending is ${classRoomPopulation((12) * 100).toFixed(2)} percent!`); //Use of toFixed to ensure I don't get messy numbers
+
+//---------------------------------------------
+
 
